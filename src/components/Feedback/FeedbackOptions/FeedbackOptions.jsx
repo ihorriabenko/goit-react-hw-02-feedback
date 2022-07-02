be58cb39047359ed3a09034b4171a2c811c4f4d2
IgorRyabenko <riabenko.igor@gmail.com>
2022-07-02 10:54:29 +0300

    proptypes

--- src/components/Feedback/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.jsx
@@ -19,7 +19,7 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 };
 
 FeedbackOptions.propTypes = {
-  options: PropTypes.array.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string.isRequired),
   onLeaveFeedback: PropTypes.func.isRequired,
 };
 
